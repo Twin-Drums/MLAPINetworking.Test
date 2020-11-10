0ce0a2f5b5f22d910ba90759388be04791514ce6
mraue <martin.raue@gmail.com>
2020-11-10 13:14:13 +0100

    Switched to MLAPI source code instead of dlls for easier debugging

---
+++ MLAPI.Networking.Test.UnityProject/Assets/Scripts/Initialization.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+
+public class Initialization : MonoBehaviour
+{
+    [SerializeField] private bool isServer;
+    [SerializeField] private string ip;
+    [SerializeField] private int port;
+
+    private void Start()
+    {
+        if(isServer)
+        {            
+            NetworkingManager.Singleton.StartServer();
+        }
+    }
+}
