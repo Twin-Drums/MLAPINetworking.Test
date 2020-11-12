52cbd24527bd784589cd406f74bd4b8509d16c59
mraue <martin.raue@gmail.com>
2020-11-12 11:01:44 +0100

    Connects neighboring grid cells in visibility grid

--- MLAPI.Networking.Test.UnityProject/Assets/Scripts/Visibility/VisibilityGrid.cs
+++ MLAPI.Networking.Test.UnityProject/Assets/Scripts/Visibility/VisibilityGrid.cs
@@ -34,6 +34,19 @@ namespace Twindrums.TheWagaduChronicles.Visibility
             public List<IVisibilityGridObject> Objects;
             public ulong Id;
             public NeighborCells Neighbors;
+
+            public void Reset()
+            {
+                this.Objects.Clear();
+                Neighbors.Top = null;
+                Neighbors.TopRight = null;
+                Neighbors.Right = null;
+                Neighbors.BottomRight = null;
+                Neighbors.Bottom = null;
+                Neighbors.BottomLeft = null;
+                Neighbors.Left = null;
+                Neighbors.TopLeft = null;
+            }
         }
 
         private float cellSize;
@@ -88,11 +101,6 @@ namespace Twindrums.TheWagaduChronicles.Visibility
             ReturnListToPool(cell);
         }
 
-        private void CleanUpNeighbors(Cell cell)
-        {
-            // todo
-        }
-
         private Cell GetCell(ulong cellId)
         {
             Cell cell = null;
@@ -109,6 +117,8 @@ namespace Twindrums.TheWagaduChronicles.Visibility
             return cell;
         }
 
+        #region Cell Neighbors
+
         private struct NeighborConnector
         {
             public GridPosition Offset;
@@ -123,14 +133,55 @@ namespace Twindrums.TheWagaduChronicles.Visibility
 
         private readonly List<NeighborConnector> neighborConnectors = new List<NeighborConnector>
         {
-            
+            new NeighborConnector(0, 1, (origin, neighbor) => { origin.Neighbors.Top = neighbor; neighbor.Neighbors.Bottom = origin; }),
+            new NeighborConnector(1, 1, (origin, neighbor) => { origin.Neighbors.TopRight = neighbor; neighbor.Neighbors.BottomLeft = origin; }),
+            new NeighborConnector(1, 0, (origin, neighbor) => { origin.Neighbors.Right = neighbor; neighbor.Neighbors.Left = origin; }),
+            new NeighborConnector(1, -1, (origin, neighbor) => { origin.Neighbors.BottomRight = neighbor; neighbor.Neighbors.TopLeft = origin; }),
+            new NeighborConnector(0, -1, (origin, neighbor) => { origin.Neighbors.Bottom = neighbor; neighbor.Neighbors.Top = origin; }),
+            new NeighborConnector(-1, -1, (origin, neighbor) => { origin.Neighbors.BottomLeft = neighbor; neighbor.Neighbors.TopRight = origin; }),
+            new NeighborConnector(-1, 0, (origin, neighbor) => { origin.Neighbors.Left = neighbor; neighbor.Neighbors.Right = origin; }),
+            new NeighborConnector(-1, 1, (origin, neighbor) => { origin.Neighbors.TopLeft = neighbor; neighbor.Neighbors.BottomRight = origin; }),            
         };
 
         private void SetUpNeighbors(Cell cell)
         {
             var gridPos = GetGridPosition(cell.Id);
 
+            for (int i = 0; i < neighborConnectors.Count; i++)
+            {
+                var nc = neighborConnectors[i];
+                var neighborId = GetCellId(gridPos.x + nc.Offset.x, gridPos.y + nc.Offset.y);
+
+                Cell neighbor = null;
+                if (!grid.TryGetValue(neighborId, out neighbor))
+                    continue;
+
+                nc.Connector(cell, neighbor);
+            }
         }
+
+        private void CleanUpNeighbors(Cell cell)
+        {
+            if (cell.Neighbors.Top != null)
+                cell.Neighbors.Top.Neighbors.Bottom = null;
+            if (cell.Neighbors.TopRight != null)
+                cell.Neighbors.TopRight.Neighbors.BottomLeft = null;
+            if (cell.Neighbors.Right != null)
+                cell.Neighbors.Right.Neighbors.Left = null;
+            if (cell.Neighbors.BottomRight != null)
+                cell.Neighbors.BottomRight.Neighbors.TopLeft = null;
+            if (cell.Neighbors.Bottom != null)
+                cell.Neighbors.Bottom.Neighbors.Top = null;
+            if (cell.Neighbors.BottomLeft != null)
+                cell.Neighbors.BottomLeft.Neighbors.TopRight = null;
+            if (cell.Neighbors.Left != null)
+                cell.Neighbors.Left.Neighbors.Right = null;
+            if (cell.Neighbors.TopLeft != null)
+                cell.Neighbors.TopLeft.Neighbors.BottomRight = null;
+        }
+
+        #endregion
+        #region Cell Pool
 
         private Cell GetListFromPool()
         {
@@ -139,11 +190,17 @@ namespace Twindrums.TheWagaduChronicles.Visibility
 
         private void ReturnListToPool(Cell cell)
         {
+            cell.Reset();
             // todo
         }
+
+        #endregion
+        #region Cell Id
 
         public ulong GetCellId(float x, float y) => GetCellId((int)(x / cellSize), (int)(y / cellSize));
         public static ulong GetCellId(int gridX, int gridY) => (((ulong)(uint)gridX) << 32) | ((ulong)(uint)gridY);
         public static GridPosition GetGridPosition(ulong cellId) => new GridPosition { x = (int)(cellId >> 32), y = (int)cellId };
+
+        #endregion
     }
 }
--- MLAPI.Networking.Test.UnityProject/Assets/Tests/TestVisibilityGrid.cs
+++ MLAPI.Networking.Test.UnityProject/Assets/Tests/TestVisibilityGrid.cs
@@ -11,7 +11,12 @@ namespace Tests
     public class TestVisibilityGrid
     {
         private class DummyVisibilityGridObject : IVisibilityGridObject
-        {
+        {            
+            public DummyVisibilityGridObject(float x, float y)
+            {
+                Position = new VisibilityGrid.Position { x = x, y = y };
+            }
+
             public VisibilityGrid.Position Position { get; set; }
 
             public VisibilityGrid.Cell Cell { get; set; }
@@ -22,9 +27,22 @@ namespace Tests
         {            
             int gridX = 23563;
             int gridY = -985462;
-            var id = VisibilityGrid.GetCellId(gridX, gridY);
-            Debug.LogFormat("[TestVisibilityGrid::CanConvertGridPositionToAndFromCellId] gridX={0} gridY={1} id={2}", gridX, gridY, id);
+            var id = VisibilityGrid.GetCellId(gridX, gridY);            
             var position = VisibilityGrid.GetGridPosition(id);
+            Assert.AreEqual(gridX, position.x);
+            Assert.AreEqual(gridY, position.y);
+
+            gridX = -652546;
+            gridY = 985462;
+            id = VisibilityGrid.GetCellId(gridX, gridY);            
+            position = VisibilityGrid.GetGridPosition(id);
+            Assert.AreEqual(gridX, position.x);
+            Assert.AreEqual(gridY, position.y);
+
+            gridX = -2436;
+            gridY = -234;
+            id = VisibilityGrid.GetCellId(gridX, gridY);
+            position = VisibilityGrid.GetGridPosition(id);
             Assert.AreEqual(gridX, position.x);
             Assert.AreEqual(gridY, position.y);
         }
@@ -33,8 +51,7 @@ namespace Tests
         public void CanRegisterGridObject()
         {
             var grid = new VisibilityGrid(1f);
-            var gridObject = new DummyVisibilityGridObject();
-            gridObject.Position = new VisibilityGrid.Position { x = 863567f, y = -6524565f };            
+            var gridObject = new DummyVisibilityGridObject(863567f, -6524565f);                  
             grid.RegisterObject(gridObject);
 
             Assert.NotNull(gridObject.Cell);
@@ -48,13 +65,82 @@ namespace Tests
         public void CanRemoveRegisteredGridObject()
         {
             var grid = new VisibilityGrid(1);
-            var gridObject = new DummyVisibilityGridObject();
-            gridObject.Position = new VisibilityGrid.Position { x = 147654, y = 65456 };
+            var gridObject = new DummyVisibilityGridObject(147654f, -65456f);            
             grid.RegisterObject(gridObject);
             var cell = gridObject.Cell;
             grid.UnregisterObject(gridObject);
             Assert.IsNull(gridObject.Cell);
             Assert.IsFalse(cell.Objects.Contains(gridObject));
+        }
+
+        [Test]
+        public void AddsCellNeighbors()
+        {
+            var objects = new List<DummyVisibilityGridObject>()
+            {
+                new DummyVisibilityGridObject(0,0),
+                new DummyVisibilityGridObject(0,1),
+                new DummyVisibilityGridObject(1,1),
+                new DummyVisibilityGridObject(1,0),
+                new DummyVisibilityGridObject(1,-1),
+                new DummyVisibilityGridObject(0,-1),
+                new DummyVisibilityGridObject(-1,-1),
+                new DummyVisibilityGridObject(-1,0),
+                new DummyVisibilityGridObject(-1,1),
+            };
+
+            var grid = new VisibilityGrid(1);
+
+            foreach (var item in objects)
+            {
+                grid.RegisterObject(item);
+            }
+
+            foreach (var item in objects)
+                Assert.AreEqual(item.Cell.Objects.Count, 1);
+
+            Assert.AreSame(objects[0].Cell.Neighbors.Top, objects[1].Cell);
+            Assert.AreSame(objects[1].Cell.Neighbors.Bottom, objects[0].Cell);
+            Assert.AreSame(objects[0].Cell.Neighbors.TopRight, objects[2].Cell);
+            Assert.AreSame(objects[2].Cell.Neighbors.BottomLeft, objects[0].Cell);
+            Assert.AreSame(objects[0].Cell.Neighbors.Right, objects[3].Cell);
+            Assert.AreSame(objects[3].Cell.Neighbors.Left, objects[0].Cell);
+            Assert.AreSame(objects[0].Cell.Neighbors.BottomRight, objects[4].Cell);
+            Assert.AreSame(objects[4].Cell.Neighbors.TopLeft, objects[0].Cell);
+        }
+
+        [Test]
+        public void RemovesCellNeighbors()
+        {
+            var objects = new List<DummyVisibilityGridObject>()
+            {
+                new DummyVisibilityGridObject(0,0),
+                new DummyVisibilityGridObject(0,1),
+                new DummyVisibilityGridObject(1,1),
+                new DummyVisibilityGridObject(1,0),
+                new DummyVisibilityGridObject(1,-1),
+                new DummyVisibilityGridObject(0,-1),
+                new DummyVisibilityGridObject(-1,-1),
+                new DummyVisibilityGridObject(-1,0),
+                new DummyVisibilityGridObject(-1,1),
+            };
+
+            var grid = new VisibilityGrid(1);
+
+            foreach (var item in objects)
+            {
+                grid.RegisterObject(item);
+            }
+
+            foreach (var item in objects)
+                Assert.AreEqual(item.Cell.Objects.Count, 1);
+
+            grid.UnregisterObject(objects[0]);
+            
+            Assert.IsNull(objects[1].Cell.Neighbors.Bottom);            
+            Assert.IsNull(objects[2].Cell.Neighbors.BottomLeft);            
+            Assert.IsNull(objects[3].Cell.Neighbors.Left);            
+            Assert.IsNull(objects[4].Cell.Neighbors.TopLeft);
         }
     }
 }
