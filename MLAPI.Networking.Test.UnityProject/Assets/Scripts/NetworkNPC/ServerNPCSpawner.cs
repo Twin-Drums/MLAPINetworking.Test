938ec8ec7b2a65e3ec3520031904e9fae681dcdf
mraue <martin.raue@gmail.com>
2020-11-16 10:24:26 +0100

    Dynamically spawn NPCs on server start

---
+++ MLAPI.Networking.Test.UnityProject/Assets/Scripts/NetworkNPC/ServerNPCSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using MLAPI;
+
+namespace Twindrums.TheWagaduChronicles.NetworkNPC
+{
+    public class ServerNPCSpawner : NetworkedBehaviour
+    {
+        [SerializeField] private GameObject prefab;
+        [SerializeField] private int amount = 100;
+        [SerializeField] private Bounds bounds;
+
+        public override void NetworkStart()
+        {
+            Debug.Log("[ServerNPCSpawner::NetworkStart]");
+            if(IsServer)
+            {
+                Debug.Log("[ServerNPCSpawner::NetworkStart] Spawning " + 100 + " NPCs");
+                for (int i = 0; i < amount; i++)
+                {
+                    var pos = new Vector3(UnityEngine.Random.Range(bounds.min.x, bounds.max.x), UnityEngine.Random.Range(bounds.min.y, bounds.max.y), UnityEngine.Random.Range(bounds.min.z, bounds.max.z));
+                    GameObject go = Instantiate(prefab, pos, Quaternion.identity);
+                    var no = go.GetComponent<NetworkedObject>();
+                    no.Spawn();                    
+                }
+            }
+        }
+    }
+}
